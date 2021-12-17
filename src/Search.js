c2aad289195d880891717c9b080a10ab376c4eda
jaeminLee524 <rkwoals524@hanmail.net>
2021-12-17 16:46:38 +0900

    검색창 디자인

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import './Search.css';
+import styled from 'styled-components';
+
+const SearchBox = styled.div`
+    padding: 10px;
+    position: absolute;
+    left: 100%;
+    transform: translate(-100%, 0%);
+    background-color: rgba(0, 0, 0, 0.2);
+    border: 1px solid rgb(250, 250, 250);
+    border-radius: 30px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+`;
+
+const SearchBlock = styled.input`
+    background-color: rgba(0, 0, 0, 0);
+    color: rgb(250, 250, 250);
+    padding: 0px;
+    width: 180px;
+    border: none;
+    outline: none;
+    float: left;
+    font-size: 1rem;
+    line-height: 30px;
+`;
 
 const Search = ({ movies, setMoviesCopy, reset }) => {
     const [value, setValue] = useState('');
@@ -26,15 +51,15 @@ const Search = ({ movies, setMoviesCopy, reset }) => {
         );
     }, [value]);
     return (
-        <>
-            <input
+        <SearchBox>
+            <SearchBlock
                 className="searchBar"
                 type="text"
                 value={value}
                 onChange={handleChange}
                 placeholder="제목을 입력해주세요"
             />
-        </>
+        </SearchBox>
     );
 };
 
