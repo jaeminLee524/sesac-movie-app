756b4825e2d29e4f2bc0815e0ff565a7f4ab272a
jaeminLee524 <rkwoals524@hanmail.net>
2021-12-18 10:05:03 +0900

    새로고침 스크롤 위치 추가

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
+// 새로고침 시 맨 위로
+// 페이지가 다시 로드되기 전 스크롤 위치가 맨 위
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
 function App() {
     return (
         <>
+            window.onbeforeunload();
             <GlobalStyle />
             <MainPage />
-
             {/* <Route exaact path="" component={} /> */}
         </>
     );
